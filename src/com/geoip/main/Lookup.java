2de7425f413d17c969ed93e6c2c7a4cbf5c755c6
kingsley02_masuku@yahoo.com <kingsley02_masuku@yahoo.com>
2017-02-01 16:43:14 +0200

    added springboot

--- src/com/geoip/main/Lookup.java
+++ src/com/geoip/main/Lookup.java
@@ -3,14 +3,24 @@ package com.geoip.main;
 import java.io.IOException;
 import java.util.List;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.maxmind.geoip.Country;
 import com.maxmind.geoip.LookupService;
 
+@RestController
+@EnableAutoConfiguration
 public class Lookup {
 
 	private List<IPLocation> locations;
 	
-
+	public Lookup() {
+		
+	}
+	
 	public Lookup(List<IPLocation> locations) {
 		this.locations = locations;
 	}
@@ -32,5 +42,14 @@ public class Lookup {
 		}
 		
 	}
+	
+	@RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+	
+	public static void main(String[] args) throws Exception {
+        SpringApplication.run(Lookup.class, args);
+    }
 
 }
