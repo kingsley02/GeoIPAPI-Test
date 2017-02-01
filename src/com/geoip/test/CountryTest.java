6283549e1390e47419e55b92a5764f5f550d10d6
Tshepo Ramatsui <tshepor@thoughtworks.com>
2017-02-01 14:54:59 +0200

    added geoip and junit dependencies

---
+++ src/com/geoip/test/CountryTest.java
@@ -0,0 +1,14 @@
+package com.geoip.test;
+
+import org.junit.Test;
+
+import com.maxmind.geoip.LookupService;
+
+public class CountryTest {
+	
+	@Test
+	public static void main(String[] args){
+		LookupService x = null;
+	}
+
+}
